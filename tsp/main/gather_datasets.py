bcae489e6a8ce92ab9db42b769ef6465c7839cfa
AinL <gmlwns5176@gmail.com>
2023-11-10 21:21:04 +0900

    update

--- tsp/main/gather_datasets.py
+++ tsp/main/gather_datasets.py
@@ -3,16 +3,5 @@ import os
 os.makedirs('cache/repos')
 os.makedirs('cache/dataset')
 
-def gather_go_codec():
-    """
-    git clone https://github.com/ugorji/go go-codec
-    cd go-codec/codec
-    git checkout XXX
-    go mod tidy
-    go test
-    """
-    pass
-
-
 if __name__ == '__main__':
     gather_go_codec()
